fb7fb58f8487151441e0a80c8cc5f717d244d5f8
Haziq Nordin <hbnordin2@gmail.com>
2018-06-06 13:39:56 +1000

    Fixed q2

--- 2017/exam2017q2.py
+++ 2017/exam2017q2.py
@@ -1,58 +1,74 @@
+memoize = {}
+
+def val(card, cardsLeft, cardsPlaced):
+    """
+    global depth
+    print(card, cardsLeft, cardsPlaced)
+    depth += 1
+    if depth == 200:
+        exit()
+    """
+
+    # Get where you can put cards
+    emptyIndices = []
+    for ind, i in enumerate(cardsPlaced):
+        if i == -1:
+            emptyIndices.append(ind)
+
+    # If you're on the last card, just put it down and finish
+    if len(emptyIndices) == 1:
+        newCardsPlaced = [j for j in cardsPlaced]
+        newCardsPlaced[emptyIndices[0]] = card
+
+        tops = 10 * newCardsPlaced[0] + newCardsPlaced[1]
+        bottoms = 10 * newCardsPlaced[2] + newCardsPlaced[3]
+
+        return tops * bottoms
+
+    # This is just the memoize stuff
+    key = (card, str(cardsLeft), str(cardsPlaced))
+    if key in memoize:
+        return memoize[key][0]
+
+    # Take the card you drew out of the pack of cards left
+    newCardsLeft = [i for i in cardsLeft]
+    newCardsLeft.remove(card)
+
+    assert len(newCardsLeft) + 1 == len(cardsLeft)
+    assert card not in newCardsLeft
+
+    # Get the value of placing the card you drew in all the spots you can
+    valuesOfIndices = []
+    indices = []
+
+    for i in emptyIndices:
+
+        assert -1 == cardsPlaced[i]
+
+        # Put the card you drew in the empty index for this iteration
+        newCardsPlaced = [j for j in cardsPlaced]
+        newCardsPlaced[i] = card
+
+        # Get the expected value of placing the card in this index
+        valuesOfAllNextCards = []
+        for nextCard in newCardsLeft:
+            valueIfYouPullNextCard = val(nextCard, newCardsLeft, newCardsPlaced)
+            valuesOfAllNextCards.append(valueIfYouPullNextCard)
+
+        valuesOfIndices.append(sum(valuesOfAllNextCards)/len(newCardsLeft))
+        indices.append(i)
+
+    assert len(valuesOfIndices) <= 4
+
+    bestVal = min(valuesOfIndices)
+
+    memoize[key] = (min(valuesOfIndices), indices[valuesOfIndices.index(bestVal)])
+
+    return min(valuesOfIndices)
+
 cardsPlaced = [-1, -1, -1, -1]
 cardsLeft = [0,1,2,3,4,5,6,7,8,9]
 cardsDrawn = 0
+print(val(cardsDrawn, cardsLeft, cardsPlaced))
 
-memoize = {}
-
-def val(cardsDrawn, cardsLeft, cardsPlaced):
-    if cardsDrawn == 10:
-        tops = int(str(cardsPlaced[0]) + str(cardsPlaced[1]))
-        bottoms = int(str(cardsPlaced[2]) + str(cardsPlaced[3]))
-        return tops * bottoms
-
-    key = (cardsDrawn, str(cardsLeft), str(cardsPlaced))
-
-    if key in memoize:
-        return memoize[key]
-
-    emptyIndices = []
-    for i in range(4):
-        if cardsPlaced[i] == -1:
-            emptyIndices.append(i)
-
-    if len(emptyIndices) == 0:
-        tops = int(str(cardsPlaced[0]) + str(cardsPlaced[1]))
-        bottoms = int(str(cardsPlaced[2]) + str(cardsPlaced[3]))
-        return tops * bottoms
-
-    expectedValueForEachAction = []
-
-    # Work out the values of each of the cards we can draw that are left
-    for i in cardsLeft:
-        
-        # Take the card out
-        remainingCards = [card for card in cardsLeft]
-        remainingCards.remove(i)
-
-        # Value of all the actions you can take.
-        valueOfActions = []
-
-        # Work out the value of placing the card in each slot
-        for placeLocation in emptyIndices:
-            resultOfPlace = [card for card in cardsPlaced]
-            resultOfPlace[placeLocation] = i
-            valueOfActions.append(val(cardsDrawn + 1, remainingCards, resultOfPlace))
-        expectedValueForEachAction.append(min(valueOfActions))
-
-        # Work out the value of not placing the card, if you have the choice
-        if len(emptyIndices) <= len(remainingCards):
-            valueOfActions.append(val(cardsDrawn + 1, remainingCards, cardsPlaced))
-
-        # Assume you make the best choice about what to do with the card
-        expectedValueForEachAction.append(min(valueOfActions))
-
-    memoize[key] = sum(expectedValueForEachAction)/len(expectedValueForEachAction)
-
-    return sum(expectedValueForEachAction)/len(expectedValueForEachAction)
-
-print(val(0, cardsLeft, cardsPlaced))
+print(memoize)
